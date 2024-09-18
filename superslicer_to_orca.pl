44a84423fd5bc1657cfc10cad10f245407cb8ce1
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2024-09-18 23:50:48 +0000

    gpt

--- superslicer_to_orca.pl
+++ superslicer_to_orca.pl
@@ -2,6 +2,10 @@
 use strict;
 use warnings;
 no warnings "exiting";
+use POSIX qw(setlocale LC_ALL);
+
+# Set locale to a supported one
+setlocale(LC_ALL, "en_US.UTF-8");
 
 use Getopt::Long;
 use File::Basename;
